9ba09a32ad6d06d421a64d17859c881009d319b2
RybinGlebRG <32393520+RybinGlebRG@users.noreply.github.com>
2017-10-09 21:25:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 import telepot.telepot as tp
 from telepot.telepot.loop import MessageLoop
-bot = tp.Bot('463574165:AAHCrL8DnLIJs85koM6zKsVCOsF0Gxjl2yc')
+bot = tp.Bot('TOKEN')
 #response = bot.getUpdates()
 
 
