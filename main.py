adbaea29f6d31866a56501525af127c776b240b1
RybinGlebRG <32393520+RybinGlebRG@users.noreply.github.com>
2017-10-10 16:07:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+-import time
+ -import telepot.telepot as tp
+ -from telepot.telepot.loop import MessageLoop
+ -bot = tp.Bot('TOKEN')
+ -#response = bot.getUpdates()
+ -
+ -
+ -def handle(msg):
+ -    content_type, chat_type, chat_id = tp.glance(msg)
+ -    print(content_type, chat_type, chat_id)
+ -
+ -    if content_type == 'text':
+ -        bot.sendMessage(chat_id, msg['text'])
+ -
+ -MessageLoop(bot,handle).run_as_thread()
+ -print ('Listening ...')
+ -
+ -while(1):
+ -    time.sleep(10)
