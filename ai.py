f477a0cf9fb2f5b4f6cde1c12aa29a58475236a4
RybinGlebRG <32393520+RybinGlebRG@users.noreply.github.com>
2017-10-21 20:27:34 +0300

    Added more interaction with player

--- ai.py
+++ ai.py
@@ -3,53 +3,96 @@ import psycopg2
 class AI():
     conn = psycopg2.connect(
         database="WORDS",
-        #Change to somwthing more secure
         user="postgres",
         password="postgres",
         host="localhost",
         port="5432"
     )
 
+    status=False
+
+    def IsGameStarted(self):
+        return  self.status
+
+    def startGame(self):
+        self.status=True
+        self.deleteUsedWords()
+
+    def closeGame(self):
+        self.status=False
+
     def __init__(self):
-        cursor=self.conn.cursor()
-        cursor.execute("delete from used")
-        self.conn.commit()
+        self.deleteUsedWords()
 
-
+    def deleteUsedWords(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute("delete from used")
+            self.conn.commit()
 
     def getUsedWords(self):
+        with self.conn.cursor() as cursor:
+            cursor.execute("select word from used")
+            res=cursor.fetchall()
+            return res
+
+    def tupleToString(self,tup):
         used = ""
-        cursor = self.conn.cursor()
-        cursor.execute("select word from used")
-        while (1):
-            res = cursor.fetchone()
-            if (res == None):
+        print(tup)
+        for elem in tup:
+            if (elem == None):
                 break
             else:
-                used += res[0] + ", "
+                used +="'"+ elem[0] + "', "
         return used[:-2]
+
+    def IsUsed(self,wrd):
+        tup=self.getUsedWords()
+        for elem in tup:
+            if elem[0]==wrd:
+                return True
+        return False
 
 
     def addUsedWord(self,wrd):
-        cursor = self.conn.cursor()
-        cursor.execute("insert into used(word) values('" + wrd + "')")
-        self.conn.commit()
+        with self.conn.cursor() as cursor:
+            cursor.execute("insert into used(word) values('" + wrd + "')")
+            self.conn.commit()
 
     def answer(self, str):
-            answer=""
+        if (str=="/startGame"):
+            self.status=True
+            return "It's your move"
+        if self.IsGameStarted():
+            return self.gameProcess(str)
+        else:
+            return self.idleChat(str)
 
-            cursor = self.conn.cursor()
-            cursor.execute("select max(color) from colors where upper(substr(color,1,1))='"+str[-1:].upper()+"' and upper(color) not in ('"+ AI.getUsedWords(self).upper()+"')")
-            res = cursor.fetchone()
-            if (res[0]==None):
-                answer="Have Losed"
-            else:
-                answer=res[0]
-                AI.addUsedWord(self,answer)
+    def idleChat(self,str):
+        return "Game isn't started"
+
+    def gameProcess(self,str):
+        answer = ""
+
+        if self.IsUsed(str):
+            answer = "That word have already been used"
             return answer
+        res = []
+        self.addUsedWord(str)
+        cursor = self.conn.cursor()
+        used = self.tupleToString(self.getUsedWords()).upper()
+        with self.conn.cursor() as cursor:
+            cursor.execute("select max(color) from colors where upper(substr(color,1,1))='" + str[-1:].upper() + "' and upper(color) not in (" + used + ")")
+            res = cursor.fetchone()
+        if (res[0] == None):
+            answer = "Have Losed"
+            self.closeGame()
+        else:
+            answer = res[0]
+            self.addUsedWord(answer)
+        return answer
 
     def close(self):
         self.conn.close();
 
     def __exit__(self, exception_type, exception_value, traceback):
-        self.conn.close();
+        self.close()
