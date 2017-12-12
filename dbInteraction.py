43dbca7834c3bbd0edeca634dbb3e9189ac66a5f
nectostr <bastinda96@gmail.com>
2017-12-12 21:39:29 +0300

    hide logs of db

--- dbInteraction.py
+++ dbInteraction.py
@@ -39,7 +39,7 @@ class DBInteraction():
 			cur_env = os.environ['DATABASE_URL']
 
 		elif state.owner == 'liuba':
-			cur_env = "postgres://hkuuzysgzvxyvi:13c076bec401203a17e0f8ad0ce4dd166763f503852be02242fc92d6af2b56be@ec2-54-243-47-252.compute-1.amazonaws.com:5432/d8pt0b38lc0vem"
+			cur_env = state.liubas_db
 		else:
 			cur_env = "postgres://postgres:postgres@127.0.0.1:5432/WRDS"
 
