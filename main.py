e0b4c9b284624000cb3e9a26b73de7e691aa76b4
Gleb <rybin.gleb.r.g@gmail.com>
2019-02-27 21:37:38 +0300

    Testing correct work

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from Logic import logic
 from TelegramInterface import ui
 from ThreadSpread import buffer
 
-buffer= buffer.Buffer()
-lock=th.Lock()
+buffer = buffer.Buffer()
+lock = th.Lock()
 
-t1= ui.UI(buffer, lock)
-t2= logic.Logic(buffer, lock)
+t1 = ui.UI(buffer, lock)
+t2 = logic.Logic(buffer, lock)
 
 t1.start()
 t2.start()
